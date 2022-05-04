fc001d4dbb0cf5a1f19fb80787cead8e744f4108
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2022-05-04 11:31:42 +0900

    add ddu sources

--- .config/nvim/lua/ui/ddu.lua
+++ .config/nvim/lua/ui/ddu.lua
@@ -17,6 +17,9 @@ call('ddu#custom#patch_global', {
     kindOptions = {
         file = {
             defaultAction = 'open'
+        },
+        command_history = {
+            defaultAction = 'execute'
         }
     },
     uiParams = {
@@ -33,6 +36,13 @@ end, { noremap = true, silent = true, nowait = true })
 keymap.set('n', '<Space>b', function()
     vim.fn['ddu#start']({ sources = { { name = 'buffer' } } })
 end, { noremap = true, silent = true, nowait = true })
+keymap.set('n', '<Space>r', function()
+    vim.fn['ddu#start']({ sources = { { name = 'mr' } } })
+end, { noremap = true, silent = true, nowait = true })
+keymap.set('n', '<Space>c', function()
+    vim.fn['ddu#start']({ sources = { { name = 'command_history' } } })
+end, { noremap = true, silent = true, nowait = true })
+
 
 -- ddu-ff
 api.nvim_create_autocmd('FileType', {
@@ -45,6 +55,7 @@ api.nvim_create_autocmd('FileType', {
             { key = '<CR>', action = 'itemAction' },
             { key = 'i', action = 'openFilterWindow' },
             { key = 'q', action = 'quit' },
+            { key = '<ESC>', action = 'quit' },
         }
         for _, keymap_setting in pairs(keymap_settings) do
             api.nvim_buf_set_keymap(0, 'n', keymap_setting.key, create_cmd_name(keymap_setting.action), { silent = true, noremap = true })
@@ -59,5 +70,6 @@ api.nvim_create_autocmd('FileType', {
         api.nvim_buf_set_keymap(0, 'i', '<CR>', '<Esc>:close<CR>', { silent = true, noremap = true })
         api.nvim_buf_set_keymap(0, 'n', '<CR>', ':close<CR>', { silent = true, noremap = true })
         api.nvim_buf_set_keymap(0, 'n', 'q', ':close<CR>', { silent = true, noremap = true })
+        api.nvim_buf_set_keymap(0, 'n', '<ESC>', ':close<CR>', { silent = true, noremap = true })
     end
 })
