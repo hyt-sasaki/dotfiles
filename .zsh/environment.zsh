09e5c7179c4f8f8aacee7234bed590daf12345d9
hayato-sasaki-biz <hayato.sasaki@bizreach.co.jp>
2026-03-22 15:10:03 +0900

    update

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -1,2 +1,2 @@
-
+export EDITOR="vim"
 
--- .zsh/post_environment.zsh
+++ .zsh/post_environment.zsh
@@ -1,2 +1,4 @@
 export GITHUB_TOKEN=$(gh auth token)
+export PATH="$HOME/.local/bin:$PATH"
 
+source ~/.safe-chain/scripts/init-posix.sh # Safe-chain Zsh initialization script
